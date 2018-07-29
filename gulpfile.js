4fc9bf199882a488c34ffe999024feb87d8964b3
nasangw <good617boy@gmail.com>
2018-07-30 02:08:01 +0900

    Add - initialize project.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cached = require('gulp-cached');
+const sassPartialsImported = require('gulp-sass-partials-imported');
+
+let scss_dir = 'src/scss/';
+let includePaths = ['src/scss/vendors'];
+
+gulp.task('sass', () => {
+	gulp.src('src/scss/**/*.scss')
+		.pipe(cached('sassfiles'))
+		.pipe(sassPartialsImported(scss_dir, includePaths))
+		.pipe(sass({ includePaths: scss_dir }).on('error', sass.logError))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/*.scss', ['sass'])
+});
